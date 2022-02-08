d24cd889674b2a007bebf79a439eaf46092857e4
cbmeeks <cbmeeks@gmail.com>
2022-02-08 11:54:50 -0500

    Not configured correctly.  Need to fix.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function Panel({ title, width, children, className }) {
-    return (
-        <div className={`font-[Poppins] ${width ?? ""} ${className ?? ""}`}>
-            <div
-                className={`rounded-t-lg border-t-2 border-l-2 border-r-2 border-gray-200 bg-blue-600 pl-4 pt-2 pb-2 text-lg font-semibold text-blue-100 shadow`}>
-                {title}
+export default class Panel extends Component({title, width, children, className}) {
+    render({title, width, children, className}) {
+        return (
+            <div className={`font-[Poppins] ${width ?? ""} ${className ?? ""}`}>
+                <div
+                    className={`rounded-t-lg border-t-2 border-l-2 border-r-2 border-gray-200 bg-blue-600 pl-4 pt-2 pb-2 text-lg font-semibold text-blue-100 shadow`}>
+                    {title}
+                </div>
+                <div
+                    className={`border-2 border-gray-200 bg-white px-4 py-2 shadow`}>
+                    {children}
+                </div>
             </div>
-            <div
-                className={`border-2 border-gray-200 bg-white px-4 py-2 shadow`}>
-                {children}
-            </div>
-        </div>
-    );
+        );
+    }
 }
