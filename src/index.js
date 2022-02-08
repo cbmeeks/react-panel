c6d987e93ef9ba401f5534b7a6ffa92576f64d96
cbmeeks <cbmeeks@gmail.com>
2022-02-08 11:01:04 -0500

    Initial code

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Panel({ title, width, children, className }) {
+    return (
+        <div className={`font-[Poppins] ${width ?? ""} ${className ?? ""}`}>
+            <div
+                className={`rounded-t-lg border-t-2 border-l-2 border-r-2 border-gray-200 bg-blue-600 pl-4 pt-2 pb-2 text-lg font-semibold text-blue-100 shadow`}>
+                {title}
+            </div>
+            <div
+                className={`border-2 border-gray-200 bg-white px-4 py-2 shadow`}>
+                {children}
+            </div>
+        </div>
+    );
+}
